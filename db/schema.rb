2d8caf5685769304161d7f0037cb25dd0965e389
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-30 16:28:03 +0000

    adding instructions to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030145410) do
+ActiveRecord::Schema.define(version: 20151030162740) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151030145410) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.boolean  "vegetarian"
+    t.text     "instructions"
   end
 
 end
