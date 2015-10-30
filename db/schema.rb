0ebfa5000254d41931629d3005576abc2b85fd6a
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-30 14:47:23 +0000

    db migrate jointable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030141657) do
+ActiveRecord::Schema.define(version: 20151030143119) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20151030141657) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "kind"
+  end
+
+  create_table "ingredients_recipes", id: false, force: :cascade do |t|
+    t.integer "ingredient_id", null: false
+    t.integer "recipe_id",     null: false
   end
 
   create_table "recipes", force: :cascade do |t|
